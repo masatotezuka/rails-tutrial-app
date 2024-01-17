8ad08093ea54860e5f7238267c742de81a8ea338
masatotezuka <tez.0731.mst@gmail.com>
2024-01-17 09:18:01 +0900

    update controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(title: "...", body: "...")
 
@@ -20,4 +24,18 @@ class ArticlesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
